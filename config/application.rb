9434b6b4d0ebfc2b895ec7eaf28a0d1981327225
Collin Clarke <collinclarke@gmail.com>
2018-12-22 16:18:12 -0800

    Fix origins

--- config/application.rb
+++ config/application.rb
@@ -11,14 +11,13 @@ module MovieList
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    config.middleware.use Rack::Cors do
+    config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins ['http://localhost:4200', 'https://collinclarke.github.io']
-        resource '*',
-                 headers: :any,
-                 :methods => [:delete, :post, :patch, :get, :options]
+        origins 'http://localhost:4200', 'https://collinclarke.github.io'
+        resource '*', headers: :any, methods: [:get, :post, :patch, :options, :delete]
       end
     end
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
